4dd7d430f0e7cd8744b8b785c653e110404dbbc0
softboy <zzu_softboy@163.com>
2017-04-12 16:17:38 +0800

    add AddCompilerFlags macro

---
+++ cmake/modules/AddCompilerFlags.cmake
@@ -0,0 +1,11 @@
+include(CheckCXXCompilerFlag)
+
+macro(notify_add_compiler_flags)
+   foreach(flag ${ARGN})
+      string(REGEX REPLACE "[-.+/:= ]" "_" _flag_esc ${flag})
+      check_cxx_compiler_flag("${flag}" check_cxx_compiler_flag_${_flag_esc})
+      if(check_cxx_compiler_flag_${_flag_esc})
+         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
+      endif()
+   endforeach()
+endmacro()
